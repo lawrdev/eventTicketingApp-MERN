d47a2d371192c08df510327e6eea2da4a95921b4
Unorthodox <lawrdev@gmail.com>
2022-10-04 11:17:02 +0100

    updated register user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -13,7 +13,40 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error('Please include all fields')
     }
-    res.send('Register route here')
+
+    //  if name, email and password are valid
+    // check if user already exsits
+    const userExists = await User.findOne({ email })
+    // method that finds one document matching the argument ( email: email )
+
+    if (userExists) {
+        throw new Error('User already exists')
+    }
+
+    // hash the password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // creae user
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    // if user was created, we send a new json data back
+    if (user) {
+        // status(201) means susccessful and something added
+        res.status(201).json({
+            // '_id' is how mongoDB stores id so,
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
 })
 
 // @desc Login a user
