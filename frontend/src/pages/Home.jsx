59db131eb914632e684a86a8ac1efb02aacaf251
Unorthodox <lawrdev@gmail.com>
2022-10-06 22:17:56 +0100

    updated UI

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,40 @@
-// import React from 'react'
+import { useNavigate } from "react-router-dom";
+import Button from "@mui/material/Button";
+import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
+import ViewDayOutlinedIcon from "@mui/icons-material/ViewDayOutlined";
 
 export function Home() {
-    return (
-        <div>Home</div>
-    )
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <section className="text-center mb-10">
+        <h3 className="headerTitle">What do you want help with?</h3>
+        <p className="headerSubTitle">Please choose an option below</p>
+      </section>
+
+      <section className="flex flex-col gap-2">
+        <Button
+          fullWidth
+          variant="outlined"
+          onClick={() => navigate("/new-ticket")}
+          sx={{ textTransform: "none" }}
+          className="!font-bold !border-2"
+          startIcon={<AddCircleOutlineOutlinedIcon />}
+        >
+          Create New Ticket
+        </Button>
+        <Button
+          fullWidth
+          variant="contained"
+          onClick={() => navigate("/tickets")}
+          sx={{ textTransform: "none" }}
+          className="!font-bold"
+          startIcon={<ViewDayOutlinedIcon />}
+        >
+          View My Tickets
+        </Button>
+      </section>
+    </>
+  );
 }
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import AccountCircle from "@mui/icons-material/AccountCircle";
+import Button from "@mui/material/Button";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import AlternateEmailOutlinedIcon from "@mui/icons-material/AlternateEmailOutlined";
@@ -12,40 +12,64 @@ import InputLabel from "@mui/material/InputLabel";
 import InputAdornment from "@mui/material/InputAdornment";
 import Input from "@mui/material/Input";
 import IconButton from "@mui/material/IconButton";
+import { Link } from "react-router-dom";
+
+import { useSelector, useDispatch } from "react-redux";
+import { register } from "../features/auth/authSlice";
 
 export function Register() {
   const [showPassword, setShowPassword] = useState(false);
   const [formData, setFormData] = useState({
-    name: " ",
+    name: "",
     email: "",
     password: "",
-    // for password checks
-    password2: "",
   });
-  const { name, email, password, password2 } = formData;
+  const { name, email, password } = formData;
 
-  const onChange = () => {
-    console.log("hey");
+  // store dispatch
+  const dispatch = useDispatch();
+
+  // destructure values from the 'auth' global state
+  const { user, isLoading, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+
+  // update data onchange
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
   };
+
   const handleClickShowPassword = () => {
     setShowPassword(!showPassword);
   };
   const handleMouseDownPassword = (event) => {
     event.preventDefault();
   };
+  // on submit of form
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    dispatch(
+      register({
+        name,
+        email,
+        password,
+      })
+    );
+  };
 
   return (
     <>
-      <section className="text-center">
+      <section className="text-center mb-6">
         <div>
-          <h3 className="font-semibold text-lg">REGISTER</h3>
-          <p className="text-gray-900">You're almost there...</p>
+          <h3 className="headerTitle">REGISTER</h3>
+          <p className="headerSubTitle">You're almost there</p>
         </div>
       </section>
 
-      <section className="text-center my-4">
-        <form className="max-w-lg mx-auto">
-          <div className=" w-full">
+      <section className="text-center">
+        <form className="max-w-lg mx-auto" onSubmit={onSubmit}>
+          <div className="formWrapper w-full">
             <Box
               className="mb-4"
               sx={{ display: "flex", alignItems: "flex-end" }}
@@ -54,11 +78,15 @@ export function Register() {
                 sx={{ color: "action.active", mr: 1, my: 0.5 }}
               />
               <TextField
+                required
                 fullWidth
                 id="name"
-                label="Name"
+                label="Enter your name"
+                name="name"
+                value={name}
                 variant="standard"
                 color="primary"
+                onChange={onChange}
               />
             </Box>
 
@@ -70,29 +98,32 @@ export function Register() {
                 sx={{ color: "action.active", mr: 1, my: 0.5 }}
               />
               <TextField
+                required
                 fullWidth
                 type="email"
                 id="email"
-                label="Email"
+                name="email"
+                label="Enter your email address"
+                value={email}
                 variant="standard"
                 color="primary"
+                onChange={onChange}
               />
             </Box>
 
-            <Box
-              className="mb-4"
-              sx={{ display: "flex", alignItems: "flex-end" }}
-            >
+            <Box sx={{ display: "flex", alignItems: "flex-end" }}>
               <LockOutlinedIcon
                 sx={{ color: "action.active", mr: 1, my: 0.5 }}
               />
               <FormControl variant="standard" className="w-full">
                 <InputLabel htmlFor="standard-adornment-password">
-                  Password
+                  Create a new password
                 </InputLabel>
                 <Input
+                  required
                   fullWidth
-                  id="standard-adornment-password"
+                  id="password"
+                  name="password"
                   type={showPassword ? "text" : "password"}
                   value={password}
                   onChange={onChange}
@@ -110,6 +141,26 @@ export function Register() {
                 />
               </FormControl>
             </Box>
+
+            <div className="pt-9">
+              <Button
+                fullWidth
+                disableElevation
+                type="submit"
+                variant="contained"
+              >
+                Submit
+              </Button>
+            </div>
+
+            <div className="pt-6">
+              <p>
+                Already a member?{" "}
+                <span className="text-blue-700">
+                  <Link to="/login">Log in</Link>
+                </span>
+              </p>
+            </div>
           </div>
         </form>
       </section>
