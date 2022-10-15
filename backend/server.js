9299040b0e3036ff1fc6cba128522e0f2c9965f6
Unorthodox <lawrdev@gmail.com>
2022-10-15 22:51:25 +0100

    added cloudinary to server

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const { cloudinary, cloudinaryConfig } = require('./utils/cloudinary');
 const express = require('express')
 require('colors')
 require('dotenv').config()
@@ -11,13 +12,41 @@ connectDB()
 
 const app = express()
 // allow data
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ limit: '50mb', extended: false }))
 
 // Routes
 app.use('/api/users', require('./Routes/userRoutes'))
 app.use('/api/events', require('./Routes/eventRoutes'))
 
+// cloudinary
+app.get("/get-signature", (req, res) => {
+    const timestamp = Math.round(new Date().getTime() / 1000)
+    const signature = cloudinary.utils.api_sign_request(
+        { timestamp: timestamp },
+        cloudinaryConfig.api_secret
+    )
+
+    res.json({ timestamp, signature })
+})
+
+app.post("/image-info", async (req, res) => {
+    // based on the public_id and the version that the (potentially malicious) user is submitting...
+    // cloudinary func that checks the returned version and public_id against our hidden api secret and returns an expected signature
+    const expectedSignature = cloudinary.utils.api_sign_request(
+        {
+            public_id: req.body.public_id,
+            version: req.body.version
+        }, cloudinaryConfig.api_secret
+    )
+
+    if (expectedSignature === req.body.signature) {
+        //   do someting with the id and other parameters
+        res.json({ img_id: req.body.public_id, })
+    } else {
+        res.json({ mssg: 'Not authorized' })
+    }
+})
 
 // error handler
 app.use(errorHandler)
