468b8be82b9875996a397f7a2096cef985183dd1
Unorthodox <lawrdev@gmail.com>
2022-10-22 11:51:33 +0100

    removed header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import { Routes, Route } from 'react-router-dom'
-import { Header } from './components/Header'
 import { Home } from './pages/Home'
 import { Register } from './pages/Register'
 import { Login } from './pages/Login'
@@ -7,22 +6,25 @@ import SnackBar from './components/SnackBar'
 import { useSelector } from "react-redux"
 import { CreateEvent } from './pages/CreateEvent'
 import { Profile } from './pages/Profile'
+import { EventPage } from './pages/EventPage'
+import TicketPage from './pages/TicketPage'
 
 function App() {
 
-  const { isSnackbarOpen, snackbarMessage } = useSelector((state) => state.global);
+  const { snackbarMessage } = useSelector((state) => state.global);
 
   return (<>
-    <Header />
-    <div className="max-w-4xl mx-auto px-3 sm:px-6 pb-6">
+    <div className="max-w-3xl mx-auto px-3 sm:px-6 pb-6">
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
         <Route path='/create-event' element={<CreateEvent />} />
         <Route path='/profile' element={<Profile />} />
+        <Route path='/events/:id' element={<EventPage />} />
+        <Route path='/events/:id/:uid' element={<TicketPage />} />
       </Routes>
-      <SnackBar isOpen={isSnackbarOpen} mssg={snackbarMessage} />
+      <SnackBar mssg={snackbarMessage} />
     </div>
   </>);
 }
