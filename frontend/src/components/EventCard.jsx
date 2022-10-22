6dd12cacc79892f56801c4a9e8e0a9488c46512f
Unorthodox <lawrdev@gmail.com>
2022-10-22 11:55:49 +0100

    updated UI

--- frontend/src/components/EventCard.jsx
+++ frontend/src/components/EventCard.jsx
@@ -1,98 +1,118 @@
-// import React from 'react'
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import CalendarMonthOutlinedIcon from "@mui/icons-material/CalendarMonthOutlined";
-import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
-import AccessTimeOutlinedIcon from "@mui/icons-material/AccessTimeOutlined";
+import { useSelector, useDispatch } from "react-redux";
 import Chip from "@mui/material/Chip";
-import Divider from "@mui/material/Divider";
-import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import SellOutlinedIcon from "@mui/icons-material/SellOutlined";
+import IosShareOutlinedIcon from "@mui/icons-material/IosShareOutlined";
 import Button from "@mui/material/Button";
+import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import dayjs from "dayjs";
+import Avatar from "@mui/material/Avatar";
+import DemoIMG from "../assets/img/demo.jpg";
+import Tooltip from "@mui/material/Tooltip";
 
 const cloud_name = "dqveipmsp";
 
 function EventCard({ event }) {
-  const { eventType, details, status } = event;
+  const [isBooked, setIsBooked] = useState(false);
+  const { eventType, details } = event;
+
+  const { user } = useSelector((state) => state.auth);
+
   const navigate = useNavigate();
 
-  // function viewEvent() {
-  //   navigate(`/events/${event._id}`);
-  // }
+  // check if booked
+  useEffect(() => {
+    if (user && event) {
+      const arr = event.ticket.members;
+      const checkArr = arr.find((element) => element === user._id);
+
+      // console.log(checkArr);
+      if (checkArr) {
+        setIsBooked(true);
+      }
+    }
+  }, [event, user]);
 
   return (
-    <div className="border-2 border-gray-200 shadow hover:shadow-lg rounded-b rounded-t-xl !text-start">
+    <div
+      className="border-2 border-gray-200 shadow hover:shadow-lg rounded-b rounded-t-xl !text-start h-full"
+      onClick={() => navigate(`/events/${event._id}`)}
+    >
       <div>
-        <div className="overflow-hidden relative h-40 bg-stale-300 bg-gray-500 rounded-t-xl">
-          <div className="eventCardImageWrapper">
-            <img
-              src={`https://res.cloudinary.com/${cloud_name}/image/upload/w_1200,h_260,c_fill,q_100/${details?.img_id}.jpg`}
-              alt="event banner"
-              className="eventCardImage"
-            />
+        <div className="w-fit relative">
+          <div className="overflow-hidden relative h-40 bg-stale-300 bg-gray-300 rounded-t-xl">
+            <div className="eventCardImageWrapper">
+              <img
+                src={`https://res.cloudinary.com/${cloud_name}/image/upload/w_1200,h_260,c_fill,q_100/${details?.img_id}.jpg`}
+                alt="event banner"
+                className="eventCardImage"
+              />
+            </div>
+
+            <div className="absolute top-3 left-2">
+              <div className="flex gap-1 text-gray-500 items-center bg-white py-1 px-2 rounded-md !font-bold">
+                <SellOutlinedIcon fontSize="small" />
+                <p className="text-xs">FREE</p>
+              </div>
+            </div>
+            <div className="absolute top-3 right-2 flex flex-col gap-2 cursor-pointer">
+              {user && (
+                <div className="flex gap-2 items-center text-white">
+                  {isBooked ? (
+                    <FavoriteOutlinedIcon fontSize="small" />
+                  ) : (
+                    <FavoriteBorderOutlinedIcon fontSize="small" />
+                  )}
+                </div>
+              )}
+              <div className="flex gap-2 text-white items-center">
+                <IosShareOutlinedIcon fontSize="small" />
+              </div>
+            </div>
           </div>
-          <div className="absolute bottom-0 right-1.5">
-            <h3 className="px-3 py-0.5 text-xs font-semibold text-white bg-gray-800 bg-opacity-90 m-1 rounded-lg w-fit">
-              {eventType}
-            </h3>
+
+          <div className="absolute z-50 bottom-0 right-2 translate-y-1/2 p-1 bg-white rounded-full cursor-pointer">
+            <Tooltip title={`${user.name}`}>
+              <Avatar
+                alt="Profile Avi"
+                src={DemoIMG}
+                sx={{ width: 50, height: 50 }}
+              />
+            </Tooltip>
           </div>
         </div>
 
-        <div className="px-3 pb-3">
-          <div className="my-2 flex justify-between item-center">
-            <p className="text-sm font-bold text-gray-700 ">{details.title}</p>
-            <Chip
-              className="!px-1 !no-underline"
-              color={
-                "primary"
-                // status === "Upcoming"
-                //   ? "primary"
-                //   : status === "Happening"
-                //   ? "info"
-                //   : status === "Closed"
-                //   ? "warning"
-                //   : "error"
-              }
+        <div className="px-5 pb-4 flex flex-col">
+          <div className="mt-3 flex gap-7 item-start overflow-hidden">
+            <div className="flex flex-col gap-1">
+              <p className="text-purple-400 font-bold">
+                {dayjs(details.date).format("MMM")}
+              </p>
+              <p className="font-bold">{dayjs(details.date).format("DD")}</p>
+            </div>
+
+            <div>
+              <h3 className="font-bold text-gray-600 mb-1.5">
+                {details.title}
+              </h3>
+              <p className="text-sm text-gray-500">{details.description}</p>
+            </div>
+          </div>
+
+          {/* <div className="mt-5 w-fit mx-auto">
+            <Button
+              disableElevation
+              color="primary"
+              variant="contained"
+              onClick={() => navigate(`/events/${event._id}`)}
+              sx={{ textTransform: "none" }}
               size="small"
-              label={status}
-              component="a"
-              href={`#${status}`}
-              clickable
-            />
-          </div>
-
-          <div className="mb-2 flex justify-between text-sm">
-            <div className="flex">
-              <CalendarMonthOutlinedIcon fontSize="small" />
-              <p className="ml-1">{details.date ? details.date : "Date"}</p>
-            </div>
-            <div className="flex">
-              <AccessTimeOutlinedIcon fontSize="small" />
-              <p className="ml-1">{details.time ? details.time : "Time"}</p>
-            </div>
-          </div>
-
-          <div className="mb-2 flex text-sm">
-            <LocationOnOutlinedIcon fontSize="small" />
-            <p className="ml-1">
-              {details.location ? details.location : "Location"}
-            </p>
-          </div>
-
-          {/* todo:: CHECK IF USER IS OWNER FOR BELOW */}
-          {/* <Divider /> */}
-          <div>
-            <div className="flex justify-center">
-              <Button
-                className="!rounded-3xl"
-                onClick={() => navigate(`/events/${event._id}`)}
-                sx={{ textTransform: "none" }}
-                size="small"
-                variant="outlined"
-                // startIcon={<BookmarkBorderIcon />}
-              >
-                View Event
-              </Button>
-            </div>
-          </div>
+            >
+              View Event
+            </Button>
+          </div> */}
         </div>
       </div>
     </div>
