f3a7fc2350f0adf660ad4497d34f93a939312727
Unorthodox <lawrdev@gmail.com>
2022-10-24 09:37:22 +0100

    created updateUser

--- backend/Routes/userRoutes.js
+++ backend/Routes/userRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router()
-const { registerUser, loginUser, getMe } = require('../controllers/userController')
+const { registerUser, loginUser, getMe, updateUser } = require('../controllers/userController')
 
 const { protect } = require('../middlewares/authMiddleware')
 
 router.post('/register', registerUser)
 router.post('/login', loginUser)
 router.get('/me', protect, getMe)
+router.put('/update', protect, updateUser)
 
 
 module.exports = router
