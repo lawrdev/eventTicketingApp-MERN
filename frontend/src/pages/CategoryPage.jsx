2eb7c8dea5ef35aedde15fa6761ae61b6ebaf6c8
Unorthodox <lawrdev@gmail.com>
2022-11-08 18:41:40 +0100

    chore: changes events source to RTK categoryEvents state

--- frontend/src/pages/CategoryPage.jsx
+++ frontend/src/pages/CategoryPage.jsx
@@ -1,26 +1,25 @@
 import { useState, useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { getEvents, getAllEvents } from "../features/event/eventSlice";
+import { getCategoryEvents } from "../features/event/eventSlice";
 import { snack } from "../features/global/globalSlice";
 import { Header } from "../components/Header";
 import BackButton from "../components/BackButton";
 import EventStatusTabs from "../components/EventStatusTabs";
+import Spinner from "../components/Spinner";
 
 function CategoryPage() {
-  const { publicEvents } = useSelector((state) => state.event);
+  const { categoryEvents } = useSelector((state) => state.event);
   const [loading, setLoading] = useState(true);
-  const [data, setData] = useState(null);
 
   const dispatch = useDispatch();
-  const navigate = useNavigate();
 
   const { type } = useParams();
 
   // GET ALL EVENTS - [public]
   useEffect(() => {
     setLoading(true);
-    dispatch(getAllEvents())
+    dispatch(getCategoryEvents(type))
       .unwrap()
       .then(() => {
         // if successfull
@@ -28,30 +27,17 @@ function CategoryPage() {
       })
       .catch((error) => {
         setLoading(false);
-        dispatch(snack("Error please try again"));
+        dispatch(snack(error));
       });
-  }, [dispatch]);
+  }, [dispatch, type]);
 
-  // Get event type
-  useEffect(() => {
-    let arr = [];
-    if (publicEvents) {
-      publicEvents.forEach((item) => {
-        if (item.eventType === type) {
-          arr.push(item);
-        }
-      });
-      setData(arr);
-    }
-  }, [publicEvents, type]);
-
-  // console.log(data?.length);
+  if (loading) return <Spinner />;
 
   return (
-    <>
+    <div className="px-3 sm:px-6">
       <Header />
 
-      <div className="mt-5 flex justify-between items-center">
+      <div className="mt-5 mb-9 flex justify-between items-center">
         <BackButton />
 
         <div>
@@ -59,10 +45,12 @@ function CategoryPage() {
         </div>
       </div>
 
-      <section>
-        <div className="mb-5">{data && <EventStatusTabs events={data} />}</div>
+      <section className="mb-10">
+        <div className="mb-5">
+          {categoryEvents && <EventStatusTabs events={categoryEvents} />}
+        </div>
       </section>
-    </>
+    </div>
   );
 }
 
